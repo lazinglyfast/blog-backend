c14dbcd5d8d1f1e37c9f891eef850115093f5680
lazinglyfast <lazinglyfast@gmail.com>
2023-09-19 21:07:13 +0200

    ex4.3

--- models/blog.js
+++ models/blog.js
@@ -7,6 +7,14 @@ const blogSchema = new mongoose.Schema({
   likes: Number,
 })
 
+blogSchema.set("toJSON", {
+  transform: (obj, updatedObj) => {
+    updatedObj.id = updatedObj._id
+    delete updatedObj._id
+    delete updatedObj.__v
+  }
+})
+
 const Blog = mongoose.model("Blog", blogSchema)
 
 export default Blog
---
+++ tests/list_helper.test.js
@@ -0,0 +1,7 @@
+const list_helper = require("../utils/list_helper.js")
+
+describe("list_helper", () => {
+  test("dummy always return 2", () => {
+    expect(list_helper.dummy()).toBe(1)
+  })
+})
