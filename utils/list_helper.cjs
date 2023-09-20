cf1cb3ed982367581899ac6678b9f1743d2b2bba
lazinglyfast <lazinglyfast@gmail.com>
2023-09-20 14:55:30 +0200

    with lodash

--- utils/list_helper.cjs
+++ utils/list_helper.cjs
@@ -1,3 +1,5 @@
+const _ = require("lodash")
+
 const dummy = (_blogs) => {
   return 1
 }
@@ -17,22 +19,20 @@ const most_likes = (blogs_list) => {
 }
 
 const most = (blogs, fn) => {
-  const map = new Map();
-
-  blogs.forEach(b => {
-    const value = (map.get(b.author) || 0) + fn(b)
-    map.set(b.author, value)
-  })
+  const hashmap = _.reduce(blogs, (acc, b) => {
+    const value = (acc.get(b.author) || 0) + fn(b)
+    return acc.set(b.author, value)
+  }, new Map())
 
   const init = {
     author: undefined,
     stat: 0,
   }
 
-  return Array.from(map)
-    .map(([author, stat]) => {
-      return { author, stat } // cannot shorthand this
-    }).reduce((max, b) => (max && max.stat) > b.stat ? max : b, init)
+  const entries = _.map(Array.from(hashmap), ([author, stat]) => {
+    return { author, stat } // cannot shorthand this
+  })
+  return _.reduce(entries, (max, b) => (max && max.stat) > b.stat ? max : b, init)
 }
 
 module.exports = {
