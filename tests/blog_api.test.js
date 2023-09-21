bc3e445ccd98699fa693a05f6aafc7821d805c15
lazinglyfast <lazinglyfast@gmail.com>
2023-09-21 22:06:02 +0200

    ex4.13

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -4,12 +4,12 @@ const api = supertest(require("../app.cjs"))
 const helper = require("./test_helper.cjs")
 const mongoose = require("mongoose")
 
-describe("blog api", () => {
-  beforeEach(async () => {
-    await Blog.deleteMany({})
-    await Blog.insertMany(helper.blogs)
-  })
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.blogs)
+})
 
+describe("querying many blogs", () => {
   test("returns all blogs", async () => {
     const response = await api.get("/api/blogs")
       .expect(200)
@@ -17,7 +17,9 @@ describe("blog api", () => {
 
     expect(response.body).toHaveLength(helper.blogs.length)
   })
+})
 
+describe("creation of a blog entry", () => {
   test("can insert a blog", async () => {
     const newBlog = {
       title: "Fighting patterns",
@@ -36,7 +38,9 @@ describe("blog api", () => {
     const titles = response.body.map(b => b.title)
     expect(titles).toContain(newBlog.title)
   })
+})
 
+describe("validation", () => {
   test("returned blog has id instead of _id", async () => {
     const response = await api.get("/api/blogs")
     expect(response.body[0].id).toBeDefined()
@@ -78,8 +82,25 @@ describe("blog api", () => {
       .send(newBlog)
       .expect(400)
   })
+})
 
-  afterAll(() => {
-    mongoose.connection.close()
+describe("deletion", () => {
+  test("blog can be deleted", async () => {
+    const responseBefore = await api.get("/api/blogs")
+    const blogsBefore = responseBefore.body
+    const blogToDelete = blogsBefore[0]
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+    const responseAfter = await api.get("/api/blogs")
+    const blogsAfter = responseAfter.body
+    expect(blogsAfter).toHaveLength(blogsBefore.length - 1)
+
+    const ids = blogsAfter.map(b => b.id)
+    expect(ids).not.toContain(blogToDelete.id)
   })
+})
+
+afterAll(() => {
+  mongoose.connection.close()
 })
