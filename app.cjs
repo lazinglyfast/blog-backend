103c19bb5916ab25f6dcdd2c8fd2f6726b80cee2
lazinglyfast <lazinglyfast@gmail.com>
2023-09-23 21:02:31 +0200

    ex4.18

--- app.cjs
+++ app.cjs
@@ -5,6 +5,7 @@ const cors = require("cors")
 const mongoose = require("mongoose")
 const blogRouter = require("./controllers/blogs.cjs")
 const userRouter = require("./controllers/users.cjs")
+const loginRouter = require("./controllers/login.cjs")
 const middleware = require("./utils/middleware.cjs")
 require("dotenv").config()
 
@@ -18,6 +19,7 @@ app.use(cors())
 app.use(express.json())
 app.use("/api/blogs", blogRouter)
 app.use("/api/users", userRouter)
+app.use("/api/login", loginRouter)
 app.use(middleware.errorHandler)
 
 module.exports = app
---
+++ controllers/login.cjs
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+const loginRouter = require("express").Router()
+const User = require("../models/user.cjs")
+
+loginRouter.post("/", async (req, res) => {
+  const { username, password } = req.body
+
+  const encrypted = await User.findOne({ username })
+  const passwordMatches = await bcrypt.compare(password, encrypted.passwordHash)
+  if (!passwordMatches) {
+    return res.status(400).json({ error: "password does not match" })
+  }
+  // what should the payload be?
+  const token = jwt.sign(username, process.env.SECRET)
+  res.status(200).json({ token })
+})
+
+module.exports = loginRouter
